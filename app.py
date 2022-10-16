ba9606fbcfa4800832a8fddc450d65a177690a78
Samintell <joshuackarim@gmail.com>
2022-10-15 21:21:21 -0500

    added react
    frontend setup

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #comment this on deployment
+from usrnameapi import UsernameApiHandler
 
-app = Flask("__name__")
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+CORS(app) #comment this on deployment
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(UsernameApiHandler, '/usrnameapi')
